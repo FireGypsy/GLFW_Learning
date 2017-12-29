d196edb14f7f0b754435ee0b413cb7ee39af9bb9
FireGypsy <firegypsy1@gmail.com>
2017-12-29 17:58:45 -0500

    multicolor triangle shader

--- test.cpp
+++ test.cpp
@@ -11,18 +11,19 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos, 1.0);\n"
-"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"	ourColor = aColor;\n"
 "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = ourColor;\n"
+"   FragColor = vec4(ourColor, 1.0);\n"
 "}\n\0";
 
 int main() {
@@ -92,9 +93,10 @@ int main() {
 
 	//vertex data
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, //bottom left
-		 0.5f, -0.5f, 0.0f, //bottom right
-		 0.0f,  0.5f, 0.0f  //top right
+		 //positions          //colors
+		-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, //bottom left
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, //bottom right
+		 0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f  //top right
 	};
 
 	unsigned int VBO, VAO;
@@ -106,9 +108,12 @@ int main() {
 	//copy the vertices array into a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//set the vertex attributes to pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//set the vertex position attributes to pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	//set the vertex color attributes to pointers
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	//tell OpenGL the size of the rendering window - sets location and size of window
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
@@ -130,12 +135,6 @@ int main() {
 
 		//draw the triangle
 		glUseProgram(shaderProgram); //use our shader when we want to render an object
-
-		//update the shader uniform over time
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 		glBindVertexArray(VAO); //bind the VAO
 		glDrawArrays(GL_TRIANGLES, 0, 3); //draw the triangle
